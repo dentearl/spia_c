bbdb451b677bbc82c2a6f18cfc2b99d827f281cd
Dent Earl <dearl@soe.ucsc.edu>
2012-01-18 14:38:16 -0800

    added a #define to get compilers to let us use getline()

--- src/fileHandling.c
+++ src/fileHandling.c
@@ -22,6 +22,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+#define _GNU_SOURCE // getline()
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
